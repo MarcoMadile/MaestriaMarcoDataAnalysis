a2f816cdf88b4d0ed8c8f01d5c7b3df1ba0f0f4a
Marco Madile Hjelt <99747962+MarcoMadile@users.noreply.github.com>
2022-08-13 15:21:37 -0300

    Create grafoDeCsvEncuentros.py
    Las primeras tres funciones arman los histogramas mostré en nuestra primera reunión (distribución de "grado") a partir de un csv que contenga los encuentros. La ultima función devuelve un grafo pesado en cantidad de veces que se encontraron una misma pareja (peso es 1 si hay solo un encuentro).

---
+++ grafoDeCsvEncuentros.py
@@ -0,0 +1,85 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import networkx as nx
+import ipdb
+
+#From encuentros (csv file) make a parejas (csv) that saves each couple and it returns couples 
+def parejas_from_encuentros(csvName,csvNameOut="",saveCSv=False):
+    df=pd.read_csv(csvName,sep=";")
+    dates=np.unique(df["day"])
+    parejas=[]
+    for date in dates:
+        #get all points for a date
+        dfaux=df[df["day"]==date]
+        t1=dfaux["name one"].values
+        t2=dfaux["name two"].values
+        paux=[]
+        for i in range(len(t1)):
+            paux.append(t1[i]+"_"+t2[i])
+        
+        parejas.append(np.unique(paux))
+    #make a dataframe using dates as collums names and each array of parejas as rows. Fill each column with the strings of the parejas.
+    dfparejas=pd.DataFrame(columns=dates,dtype=str)
+    for i in range(len(dates)):
+        dfparejas.iloc[:,i]=pd.Series(parejas[i])
+    #sort columns names in dataframe. This should be according to the order of dates. Fist it should be getting the dates from column names and then sort them. The column names are strings so i need to convert them to date object to sort it.
+    idx = pd.to_datetime(dfparejas.columns, errors='coerce', format='%d-%m-%y').argsort()
+    dfparejas=dfparejas.iloc[:, idx]
+    if saveCSv:
+        dfparejas.to_csv(csvNameOut,sep=";")
+    return parejas
+
+#from parejas list, return names of turtles and grade in two different arrays 
+def count_grades(parejas):
+    parejasflat=[]
+    for i in range(len(parejas)):
+        for j in range(len(parejas[i])):
+            parejasflat.append(parejas[i][j])
+    parejasflat=np.unique(parejasflat)
+    #separe parejasflat into two arrays, for each element of parejas flat separe where there is "_"
+    parejasflat1=[]
+    parejasflat2=[]
+    for i in range(len(parejasflat)):
+        parejasflat1.append(parejasflat[i].split("_")[0])
+        parejasflat2.append(parejasflat[i].split("_")[1])
+    #get uniques names 
+    names=np.unique(parejasflat1+parejasflat2)
+    conectionsForName=np.zeros(len(names))
+    for i in range(len(names)):
+        parejas_name=[]
+        for j in range(len(parejasflat1)):
+            if parejasflat1[j]==names[i]:
+                if not parejasflat2[j] in parejas_name:
+                    conectionsForName[i]+=1
+                    parejas_name.append(parejasflat2[j])
+        for j in range(len(parejasflat2)):
+            if parejasflat2[j]==names[i]:
+                if not parejasflat1[j]in parejas_name:
+                    conectionsForName[i]+=1
+                    parejas_name.append(parejasflat1[j])
+    return names,conectionsForName
+
+#from names and conectionsForname makes a histogram 
+def plot_grades(names,conectionsForName):
+    plt.bar(names,conectionsForName)
+    plt.show()
+
+#from encuentros csv it makes agraph of the connections between turtles adding weights to the edges depending on the number of meetings
+def plot_weighted_graph(encuentrosCsv):
+    df=pd.read_csv(encuentrosCsv,sep=";")
+    t1=(df["name one"].values).tolist() 
+    t2=(df["name two"].values).tolist()
+    names=np.unique(t1+t2)
+    
+    G=nx.Graph()
+    G.add_nodes_from(names)
+    for i in range(len(t1)):
+        if G.has_edge(t1[i],t2[i]):
+            G[t1[i]][t2[i]]['weight']+=1
+        else:
+            G.add_edge(t1[i],t2[i],weight=1)
+    nx.draw(G, with_labels=True)
+    plt.show()    
+    return G
+
