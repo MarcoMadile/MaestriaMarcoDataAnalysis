4bca3c35864a42f60166c37b09db1e27a0d19b76
Marco Madile Hjelt <marco.madile@ib.edu.ar>
2022-09-06 19:26:06 -0300

    Agregado slider_from_pngs, usa matplotlib slider

--- DataAnalysis/IgotoGif/slider_from_pngs.py
+++ DataAnalysis/IgotoGif/slider_from_pngs.py
@@ -4,7 +4,10 @@ import mpl_toolkits.axes_grid1
 import numpy as np 
 import glob
 import os
+import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from matplotlib.widgets import Slider, Button
+import cv2
 
 class PageSlider(matplotlib.widgets.Slider):
 
@@ -37,10 +40,8 @@ class PageSlider(matplotlib.widgets.Slider):
         divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
         bax = divider.append_axes("right", size="5%", pad=0.05)
         fax = divider.append_axes("right", size="5%", pad=0.05)
-        self.button_back = matplotlib.widgets.Button(bax, label=ur'$\u25C0$', 
-                        color=self.facecolor, hovercolor=self.activecolor)
-        self.button_forward = matplotlib.widgets.Button(fax, label=ur'$\u25B6$', 
-                        color=self.facecolor, hovercolor=self.activecolor)
+        self.button_back = matplotlib.widgets.Button(bax, label=r'$\u25C0$', color=self.facecolor, hovercolor=self.activecolor)
+        self.button_forward = matplotlib.widgets.Button(fax, label=r'$\u25B6$',color=self.facecolor, hovercolor=self.activecolor)
         self.button_back.label.set_fontsize(self.fontsize)
         self.button_forward.label.set_fontsize(self.fontsize)
         self.button_back.on_clicked(self.backward)
@@ -80,10 +81,57 @@ def draw_pngs(folder="",remove_pngs=False):
     frames = []
     for i in files:
         new_frame = mpimg.imread(i)
-        new_size = (int(new_frame.width/4),int( new_frame.height/4))
-        new_frame = new_frame.resize(new_size) 
         frames.append(new_frame)
         # Save into a GIF file that loops forever
         if remove_pngs:
             os.remove(i)
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(bottom=0.18)
+    num_pages = len(frames)
+    im = ax.imshow(frames[0])
+
+    ax_slider = fig.add_axes([0.1, 0.05, 0.8, 0.04])
+    slider = PageSlider(ax_slider, 'Page', num_pages, activecolor="orange")
+
+    def update(val):
+        i = int(slider.val)
+        im.set_data(frames[i])
+
+    slider.on_changed(update)
+    plt.axis("off")
+    plt.show() 
+
+def draw_pngs_mp_slide(folder="",remove_pngs=False):
+    filenames=folder+"/*.png"
+    files=glob.glob(filenames)
+    frames = []
+    for i in files:
+        new_frame = mpimg.imread(i) 
+        #reduce size of image to 0.7 
+        new_frame = cv2.resize(new_frame, (0,0), fx=0.7, fy=0.7)
+        frames.append(new_frame)
+        if remove_pngs:
+            os.remove(i)
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.15)
+    im = ax.imshow(frames[0])
+    allowed_values = np.arange( len(frames))
+    ax.axis("off")
+    ax_slider = plt.axes([0.15, 0.1, 0.65, 0.03])
+    slider = Slider(ax_slider, 'Frame', 0, len(frames)-1, valinit=0, valstep=1)
+    def update(val):
+        frame = slider.val
+        im.set_data(frames[int(frame)])
+    slider.on_changed(update)
+    ax_advance = plt.axes([0.8, 0.025, 0.1, 0.04])
+    button = Button(ax_advance, 'Move frames', hovercolor='0.975')
+    def advance_2(event):
+        if slider.val+2 < len(frames):
+            slider.set_val(slider.val+2)
+    button.on_clicked(advance_2)
     
+    plt.tight_layout()
+    plt.show()
+
+folder= "D:\\facultad\\IB5toCuatri\\Tesis\\MaestriaMarco\\DataAnalysis\\IgotoGif\\pngs" 
+draw_pngs_mp_slide(folder)
